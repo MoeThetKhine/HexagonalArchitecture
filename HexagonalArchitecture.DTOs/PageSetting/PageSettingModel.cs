63c4b7d2490bbc6d51c17be0ec34e78428a1b0d2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:42:29 +0630

    -

--- HexagonalArchitecture.DTOs/PageSetting/PageSettingModel.cs
+++ HexagonalArchitecture.DTOs/PageSetting/PageSettingModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace HexagonalArchitecture.DTOs.PageSetting;
 
+#region PageSettingModel
+
 public class PageSettingModel
 {
 	public int PageNo {  get; set; }
@@ -15,3 +17,5 @@ public class PageSettingModel
 		TotalCount = totalCount;
 	}
 }
+
+#endregion
