60c4e0cf43ccf460af217524acfc62dd98609ff7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 23:41:58 +0630

    Feat : Add Result

--- HexagonalArchitecture.Utils/Result.cs
+++ HexagonalArchitecture.Utils/Result.cs
@@ -2,4 +2,8 @@
 
 public class Result<T>
 {
+	public T Data { get; set; }
+	public string Message { get; set; }
+	public bool IsSuccess { get; set; }
+	public EnumStatusCode StatusCode { get; set; }
 }
