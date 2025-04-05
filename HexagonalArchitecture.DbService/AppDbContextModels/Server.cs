4353eea06a2218220c18f1fdde6f7384b4c5fd52
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:13:13 +0630

    -

---
+++ HexagonalArchitecture.DbService/AppDbContextModels/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HexagonalArchitecture.DbService.AppDbContextModels;
+
+public partial class Server
+{
+    public string Id { get; set; } = null!;
+
+    public string? Data { get; set; }
+
+    public DateTime LastHeartbeat { get; set; }
+}
