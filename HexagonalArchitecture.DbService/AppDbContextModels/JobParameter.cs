96c5c512c859b14debe2cbb117fbe7f78355548e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:20:16 +0630

    -

--- HexagonalArchitecture.DbService/AppDbContextModels/JobParameter.cs
+++ HexagonalArchitecture.DbService/AppDbContextModels/JobParameter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace HexagonalArchitecture.DbService.AppDbContextModels;
+﻿namespace HexagonalArchitecture.DbService.AppDbContextModels;
 
 public partial class JobParameter
 {
