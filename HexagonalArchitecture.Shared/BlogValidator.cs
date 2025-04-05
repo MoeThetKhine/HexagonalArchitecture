177d1e363531d13a225df74241fd0620284f2551
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 22:59:50 +0630

    Feat : Add BlogValidator

--- HexagonalArchitecture.Shared/BlogValidator.cs
+++ HexagonalArchitecture.Shared/BlogValidator.cs
@@ -1,5 +1,24 @@
-﻿namespace HexagonalArchitecture.Shared;
+﻿using FluentValidation;
+using HexagonalArchitecture.DTOs.Features.Blog;
 
-public class BlogValidator
+namespace HexagonalArchitecture.Shared;
+
+public class BlogValidator : AbstractValidator<BlogRequestModel>
 {
+
+	public BlogValidator()
+	{
+		RuleFor(x => x.BlogTitle)
+			.NotEmpty().WithMessage("Blog Title cannot be empty.")
+			.NotNull().WithMessage("Blog Title cannot be null.");
+
+		RuleFor(x => x.BlogAuthor)
+			 .NotEmpty().WithMessage("Blog Author cannot be empty.")
+			.NotNull().WithMessage("Blog Author cannot be null.");
+
+		RuleFor(x => x.BlogContent)
+		   .NotEmpty().WithMessage("Blog Content cannot be empty.")
+		   .NotNull().WithMessage("Blog Content cannot be null.");
+	}
+
 }
