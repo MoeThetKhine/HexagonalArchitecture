fb915ccd0677712c7a8d9d69c07fa0a0c9b99418
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:15:40 +0630

    -

--- HexagonalArchitecture.DbService/AppDbContextModels/ToDoList.cs
+++ HexagonalArchitecture.DbService/AppDbContextModels/ToDoList.cs
@@ -1,5 +1,7 @@
 ﻿namespace HexagonalArchitecture.DbService.AppDbContextModels;
 
+#region ToDoList
+
 public partial class ToDoList
 {
     public int TaskId { get; set; }
@@ -22,3 +24,5 @@ public partial class ToDoList
 
     public virtual TaskCategory? Category { get; set; }
 }
+
+#endregion
