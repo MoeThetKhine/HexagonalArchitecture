890b163d36dda96ff23a5990aa3b33b1c0565ece
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:38:58 +0630

    -

--- HexagonalArchitecture.DTOs/PageSetting/PageSettingModel.cs
+++ HexagonalArchitecture.DTOs/PageSetting/PageSettingModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HexagonalArchitecture.DTOs.PageSetting
+﻿namespace HexagonalArchitecture.DTOs.PageSetting
 {
 	internal class PageSettingModel
 	{
