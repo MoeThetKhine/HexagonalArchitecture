90645906b25efa62c9bcddfd65af9d5a20111ccf
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:13:04 +0630

    -

---
+++ HexagonalArchitecture.DbService/AppDbContextModels/Schema.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HexagonalArchitecture.DbService.AppDbContextModels;
+
+public partial class Schema
+{
+    public int Version { get; set; }
+}
