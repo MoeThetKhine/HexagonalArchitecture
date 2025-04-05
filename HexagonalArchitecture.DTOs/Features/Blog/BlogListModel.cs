cec0831fbd0c8004254393d40998d3622f5fbae0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 21:49:21 +0630

    -

--- HexagonalArchitecture.DTOs/Features/Blog/BlogListModel.cs
+++ HexagonalArchitecture.DTOs/Features/Blog/BlogListModel.cs
@@ -2,11 +2,16 @@
 
 namespace HexagonalArchitecture.DTOs.Features.Blog;
 
+#region BlogListModel
+
 public class BlogListModel
 {
 	public IEnumerable<BlogModel> DataLst { get; set; }
 	public PageSettingModel PageSetting { get; set; }
 }
+
+#endregion
+
 
 public class BlogListModelV1
 {
