6c6ee82528d386000104c844fc4737532f33d869
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-06 21:48:39 +0630

    -

--- HexagonalArchitecture.API/Controllers/BaseController.cs
+++ HexagonalArchitecture.API/Controllers/BaseController.cs
@@ -7,8 +7,13 @@ namespace HexagonalArchitecture.API.Controllers;
 [ApiController]
 public class BaseController : ControllerBase
 {
+
+	#region Content
+
 	public IActionResult Content(object obj)
 	{
 		return Content(obj.ToJson(), "application/json");
 	}
+
+	#endregion
 }
