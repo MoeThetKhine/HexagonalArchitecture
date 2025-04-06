f733b64960dd2ca7a93fff7e9b915c7586b9fd87
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-06 20:47:46 +0630

    Feat : Add Tbl_Blog

--- HexagonalArchitecture.Domain/Features/Blog/Tbl_Blog.cs
+++ HexagonalArchitecture.Domain/Features/Blog/Tbl_Blog.cs
@@ -1,5 +1,14 @@
-﻿namespace HexagonalArchitecture.Domain.Features.Blog;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
+namespace HexagonalArchitecture.Domain.Features.Blog;
+
+[Table("Tbl_Blog")]
 public class Tbl_Blog
 {
+	[Key]
+	public int BlogId { get; set; }
+	public string BlogTitle { get; set; }
+	public string BlogAuthor { get; set; }
+	public string BlogContent { get; set; }
 }
