bea63fde78847e6ba195b00cc0eb36fbbfd3bb06
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-06 13:37:22 +0630

    Format Code

--- HexagonalArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,6 +1,5 @@
-﻿namespace HexagonalArchitecture.Application.Features.Blog.DeleteBlog
+﻿namespace HexagonalArchitecture.Application.Features.Blog.DeleteBlog;
+
+internal class DeleteBlogCommandHandler
 {
-	internal class DeleteBlogCommandHandler
-	{
-	}
 }
