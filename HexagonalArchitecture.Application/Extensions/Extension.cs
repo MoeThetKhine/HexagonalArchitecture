68bcb658e493740babf42d531f3023e4abf639ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-06 12:23:30 +0630

    -

---
+++ HexagonalArchitecture.Application/Extensions/Extension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexagonalArchitecture.Application.Extensions
+{
+	internal class Extension
+	{
+	}
+}
