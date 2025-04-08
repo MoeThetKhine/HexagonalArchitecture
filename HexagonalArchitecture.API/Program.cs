0c8222273fc8d0eb282cf524984470ee070531ce
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 01:29:32 +0630

    Feat : Add DI Service in Program.cs

--- HexagonalArchitecture.API/Program.cs
+++ HexagonalArchitecture.API/Program.cs
@@ -1,8 +1,12 @@
+using HexagonalArchitecture.API.Extension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
