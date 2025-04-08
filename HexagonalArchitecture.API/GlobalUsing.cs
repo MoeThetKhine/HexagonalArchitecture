f3faa918bd54a97c785a00168cdc9f7efb843c06
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 01:30:03 +0630

    Feat : Add Global Using

--- HexagonalArchitecture.API/GlobalUsing.cs
+++ HexagonalArchitecture.API/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using HexagonalArchitecture.DbService.AppDbContextModels;
 global using HexagonalArchitecture.Domain.Features.Blog;
 global using HexagonalArchitecture.Infrastructure.Features.Blog;
 global using Microsoft.EntityFrameworkCore;
+global using HexagonalArchitecture.API.Extension;
--- HexagonalArchitecture.API/Program.cs
+++ HexagonalArchitecture.API/Program.cs
@@ -1,4 +1,3 @@
-using HexagonalArchitecture.API.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
 
