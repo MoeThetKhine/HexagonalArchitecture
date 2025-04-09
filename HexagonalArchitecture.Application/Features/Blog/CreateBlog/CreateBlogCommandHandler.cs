2b4a0df0825f484a1e597627c39c7435545ef0a4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 23:57:41 +0630

    Feat : Add CreateBlogCommandHandler

--- HexagonalArchitecture.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,5 +1,42 @@
-﻿namespace HexagonalArchitecture.Application.Features.Blog.CreateBlog;
+﻿using HexagonalArchitecture.Shared;
 
-public class CreateBlogCommandHandler
+namespace HexagonalArchitecture.Application.Features.Blog.CreateBlog;
+
+public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand , Result<BlogModel>>
 {
+	private readonly IBlogPort _blogPort;
+
+	public CreateBlogCommandHandler(IBlogPort blogPort)
+	{
+		_blogPort = blogPort;
+	}
+
+	public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.requestModel.BlogTitle.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+			goto result;
+		}
+
+		if(request.requestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+			goto result;
+		}
+		if (request.requestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+			goto result;
+		}
+
+		result = await _blogPort.CreateBlogAsync(request.requestModel, cancellationToken);
+
+	result:
+		return result;
+
+	}
+
 }
