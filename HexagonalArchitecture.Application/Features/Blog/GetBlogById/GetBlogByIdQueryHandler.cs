f7cc49a2fc8fa3387f94036bb0147fe07d3c38b3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 23:50:04 +0630

    Feat : Add GetBlogByIdQueryHandler

--- HexagonalArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,5 +1,28 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.GetBlogById;
 
-public class GetBlogByIdQueryHandler
+public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, Result<BlogModel>>
 {
+	private readonly IBlogPort _blogPort;
+
+	public GetBlogByIdQueryHandler(IBlogPort blogPort)
+	{
+		_blogPort = blogPort;
+	}
+
+	public async Task<Result<BlogModel>> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogPort.GetBlogByIdAsync(request.BlogId, cancellationToken);
+
+		result:
+		return result;
+
+	}
 }
