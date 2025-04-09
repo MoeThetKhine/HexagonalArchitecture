2c30e922c178a27052d0ec701415a44294e45ba3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 21:26:21 +0630

    Feat : Add GetBlogListQuery

--- HexagonalArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ HexagonalArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,5 +1,17 @@
-﻿namespace HexagonalArchitecture.Application.Features.Blog.GetBlogList;
+﻿using HexagonalArchitecture.DTOs.Features.Blog;
+using HexagonalArchitecture.Utils;
+using MediatR;
 
-public class GetBlogListQuery
+namespace HexagonalArchitecture.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQuery :  IRequest<Result<BlogListModelV1>>
 {
+	public int PageNo {  get; set; }
+	public int PageSize { get; set; }
+
+	public GetBlogListQuery(int pageNo, int pageSize)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
+	}
 }
