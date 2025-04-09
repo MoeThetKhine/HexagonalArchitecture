952cbc81473d684e2429339cc8b3ffc0fe85cd95
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-10 00:39:13 +0630

    Feat : Add DeleteBlogCommandHandler

--- HexagonalArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,5 +1,27 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.DeleteBlog;
 
-public class DeleteBlogCommandHandler
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogPort _blogPort;
+
+	public DeleteBlogCommandHandler(IBlogPort blogPort)
+	{
+		_blogPort = blogPort;
+	}
+
+	public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogPort.DeleteBlogAsync(request.BlogId, cancellationToken);
+
+		result:
+		return result;
+	}
 }
