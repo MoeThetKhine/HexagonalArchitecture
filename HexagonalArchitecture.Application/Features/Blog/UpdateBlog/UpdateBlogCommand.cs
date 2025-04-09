1d5207bca3dd11bf42b6e5639f0679d6d92e49e3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-10 00:03:57 +0630

    Feat : Add UpdateBlogCommand

--- HexagonalArchitecture.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ HexagonalArchitecture.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,5 +1,13 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.UpdateBlog;
 
-public class UpdateBlogCommand
+public class UpdateBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel requestModel { get; set; }
+	public int BlogId {  get; set; }
+
+	public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
+	{
+		this.requestModel = requestModel;
+		BlogId = blogId;
+	}
 }
