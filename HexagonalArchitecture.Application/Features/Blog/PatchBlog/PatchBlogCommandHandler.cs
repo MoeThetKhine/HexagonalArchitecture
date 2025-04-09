e61d19a430b228b9ad31928afa7811dcada92ce0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-10 00:42:42 +0630

    Feat : Add PatchBlogCommandHandler

--- HexagonalArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,5 +1,30 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.PatchBlog;
 
-public class PatchBlogCommandHandler
+public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogPort _blogPort;
+
+	public PatchBlogCommandHandler(IBlogPort blogPort)
+	{
+		_blogPort = blogPort;
+	}
+
+
+	public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogPort.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
+
 }
