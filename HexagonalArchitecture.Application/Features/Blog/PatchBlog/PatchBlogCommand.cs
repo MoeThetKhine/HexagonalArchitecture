c700d33c5f4832f69c2a466d1f64e7db3a2c3c75
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-10 00:41:20 +0630

    Feat : Add PatchBlogCommand

--- HexagonalArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ HexagonalArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,5 +1,16 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.PatchBlog;
 
-public class PatchBlogCommand
+public class PatchBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel BlogRequestModel { get; set; }
+
+	public int BlogId { get; set; }
+
+
+	public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
+	{
+		BlogRequestModel = blogRequestModel;
+		BlogId = blogId;
+	}
+
 }
