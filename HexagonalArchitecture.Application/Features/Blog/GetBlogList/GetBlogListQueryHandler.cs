886a9f30002f86842d3677d7336944713703f104
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 22:23:55 +0630

    Feat : Add GetBlogListQueryHandler

--- HexagonalArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ HexagonalArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,5 +1,45 @@
-﻿namespace HexagonalArchitecture.Application.Features.Blog.GetBlogList;
+﻿using HexagonalArchitecture.Domain.Features.Blog;
 
-public class GetBlogListQueryHandler
+namespace HexagonalArchitecture.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListModelV1>>
 {
+	private readonly IBlogPort _blogPort;
+
+	public GetBlogListQueryHandler(IBlogPort blogPort)
+	{
+		_blogPort = blogPort;
+	}
+
+
+	public async Task<Result<BlogListModelV1>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		try
+		{
+			if (request.PageNo <= 0)
+			{
+				result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageNo);
+				goto result;
+			}
+
+			if (request.PageSize <= 0)
+			{
+				result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageSize);
+				goto result;
+			}
+
+			result = await _blogPort.GetBlogListAsync(request.PageNo, request.PageSize, cancellationToken);
+
+		}
+		catch (Exception ex)
+		{
+			result = Result<BlogListModelV1>.Failure(ex);
+		}
+	result:
+		return result;
+	}
+
+
 }
