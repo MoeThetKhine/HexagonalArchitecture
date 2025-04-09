260ce96cbc983e8169135c44458e7e280e57ae14
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-10 00:46:35 +0630

    Feat : Add GetBlogAsync in Controller

--- HexagonalArchitecture.API/Controllers/Blog/BlogController.cs
+++ HexagonalArchitecture.API/Controllers/Blog/BlogController.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HexagonalArchitecture.Application.Features.Blog.GetBlogList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HexagonalArchitecture.API.Controllers.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController : ControllerBase
+public class BlogController : BaseController
 {
+	private readonly IMediator _mediator;
+
+	public BlogController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		var query = new GetBlogListQuery(pageNo, pageSize);
+		var result = await _mediator.Send(query, cancellationToken);
+
+		return Content(result);
+	}
 }
