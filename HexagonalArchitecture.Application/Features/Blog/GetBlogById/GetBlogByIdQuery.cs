4a1830b66ba04cb8dcd3d0854df36821f76f1513
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-09 23:05:09 +0630

    Feat : Add GetBlogByIdQuery

--- HexagonalArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ HexagonalArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,5 +1,11 @@
 ﻿namespace HexagonalArchitecture.Application.Features.Blog.GetBlogById;
 
-public class GetBlogByIdQuery
+public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 {
+	public int BlogId { get; set; }
+
+	public GetBlogByIdQuery(int blogId)
+	{
+		BlogId = blogId;
+	}
 }
